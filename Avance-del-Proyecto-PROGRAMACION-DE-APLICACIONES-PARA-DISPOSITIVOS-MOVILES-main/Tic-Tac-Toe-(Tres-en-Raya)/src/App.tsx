0aac96025ed4dbb2346b79f078e86211f99d9e97
JCJARAA99 <91167214+Jaraa99@users.noreply.github.com>
2024-10-28 07:28:23 -0500

    Add files via upload

---
+++ Avance-del-Proyecto-PROGRAMACION-DE-APLICACIONES-PARA-DISPOSITIVOS-MOVILES-main/Tic-Tac-Toe-(Tres-en-Raya)/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+import Juego from './components/game';  
+
+const App: React.FC = () => (  
+  <div>  
+    <h1>Tic-Tac-Toe</h1>  
+    <Juego />  
+  </div>  
+);  
+
+export default App;
